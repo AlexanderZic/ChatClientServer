2bc892dcce16ec3bcda6ec33ef7dd6daaf2e39e9
Alexander Zic <zic.alexander@gmail.com>
2023-10-27 20:56:28 -0600

    Ticket #U6_01

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer'; 
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+      </header> 
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
